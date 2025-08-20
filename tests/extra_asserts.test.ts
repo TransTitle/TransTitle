a56797c8c28dffe05bf7b729c2e43cf07b5f3fba
Peter Lyons Kehl <peter.kehl@gmail.com>
2025-08-20 11:23:23 -0700

    Test of extra_asserts and Yellow.toString()

---
+++ tests/extra_asserts.test.ts
@@ -0,0 +1,22 @@
+// Copyright (c) 2025-present TransTitle Contributors. Licensed under MIT license, Apache 2.0
+// license and BSD license.
+
+import * as tt from '../index.js';
+
+import test from "node:test";
+import assert from "node:assert";
+
+tt.flags.extra_asserts = true;
+
+test("Yellow.toString() fails an extra assert", () => {
+    var encodedWhole;
+    assert.throws(
+        () => {
+            tt.Yellow.INSTANCE.toString()
+        },
+        {
+            name: 'Error',
+            message: "Don't print/format any instance of class Yellow.",
+        }
+    );
+});
